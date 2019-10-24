7f99353b23ecdb2215d1caae2db0c6103c42c5da
Mario Luo <luokaiqiongmou@foxmail.com>
2019-10-24 18:16:12 +0800

    修复spring-boot错误跳转拦截

--- src/main/java/com/github/lkqm/auth/core/AuthInterceptor.java
+++ src/main/java/com/github/lkqm/auth/core/AuthInterceptor.java
@@ -15,18 +15,32 @@ public class AuthInterceptor extends HandlerInterceptorAdapter {
 
     private TinyAuth tinyAuth;
 
+    private static String[] excludeControllers = {
+            "org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController",
+            "org.springframework.boot.autoconfigure.web.BasicErrorController"
+    };
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         if (handler instanceof HandlerMethod) {
             HandlerMethod handlerMethod = (HandlerMethod) handler;
             String method = request.getMethod();
-            boolean isNeedTinyAuth = !isExcludeMethod(method);
+            boolean isNeedTinyAuth = !isExcludeMethod(method) && !isExcludeController(handlerMethod);
             if (isNeedTinyAuth) {
                 tinyAuth.auth(request, handlerMethod);
             }
         }
         return true;
+    }
+
+    private boolean isExcludeController(HandlerMethod handlerMethod) {
+        String className = handlerMethod.getBeanType().getName();
+        for (String exclude : excludeControllers) {
+            if (exclude.equals(className)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private boolean isExcludeMethod(String method) {
